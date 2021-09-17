1ae71cc0cda49f750a100abd8ab2650037a2b0dd
Mat <matshot03@gmail.com>
2021-09-17 16:02:28 +0200

    Cases update

--- js/script.js
+++ js/script.js
@@ -1,9 +1,49 @@
-const cases = [1,2,3,4,5,6,7,8,9,10,11,12]
-cases.forEach( element=> {
-    ReactDOM.render(<h1 class="casecontent"> {element} </h1>, document.getElementById('case1'));
-});
+const cases = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+var textpos = 1
+var playerpos = 0
+var playercase = "case" + playerpos
+
+initialize();
+
+function initialize() {
+    cases.forEach(element => {
+        ReactDOM.render(<h1 className="casecontent"> {element} </h1>, document.getElementById("case" + textpos));
+        textpos += 1
+    });
+}
+
 // const case1 = <h1 class="casecontent"> 1 </h1>
 // var randomQ = new XMLHttpRequest();
 // randomQ = randomQ.open('GET', '../questions/q1.txt');
 
 // ReactDOM.render(randomQ, document.getElementById('case1'));
+function butevent() {
+    playerpos += 1
+    playermove()
+}
+
+
+function playermove() {
+    ReactDOM.render(<img src="/img/OIE_1.png" alt="oie" ></img>, document.getElementById("case" + playerpos));
+
+}
+
+function reset() {
+    location.reload();
+}
+
+function popUp() {
+
+}
+
+function openquestion() {
+    answer = prompt("Answer this question douchebag");
+    switch (answer) {
+        case 1:
+            
+            break;
+    
+        default:
+            break;
+    }
+}
